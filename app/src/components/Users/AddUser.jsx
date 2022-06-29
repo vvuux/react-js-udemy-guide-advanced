a3e48ffb4557278d8a52d2b5cba28a2471437c12
Pham Ngoc Vu <phamngcvu21@gmail.com>
2022-06-29 10:08:09 +0700

    AddUser Component update

--- app/src/components/Users/AddUser.jsx
+++ app/src/components/Users/AddUser.jsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button"
+import styles from "./AddUser.module.css";
 
 const AddUser = props => {
+    const [enterUserName, setEnterUserName] = useState('');
+    const [enterAge, setEnterAge] = useState('');
+
     const addUserHandler = (e) => {
         e.preventDefault();
+        if (enterUserName.trim().length === 0 || enterAge.trim().length === 0){
+            return;
+        }
+        if (+enterAge < 1){
+            return
+        }
+        setEnterAge('');
+        setEnterUserName('');
     }
 
+    const userNameChangeHandler = (e) => {
+        setEnterUserName(e.target.value);
+    };
+
+    const ageChangeHandler = (e) => {
+        setEnterAge(e.target.value);
+    };
+
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username"/>
-            <label htmlFor="age">Age (Years)</label>
-            <input type="number" id="age"/>
-            <button type="submit">Add User</button>
-        </form>
+        <Card className={styles.input}>        
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input 
+                    type="text" 
+                    id="username" 
+                    onChange={userNameChangeHandler}
+                    value={enterUserName}
+                />
+                <label htmlFor="age">Age (Years)</label>
+                <input 
+                    type="number" 
+                    id="age" 
+                    onChange={ageChangeHandler}
+                    value={enterAge}
+                />
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+
     )
 };
 
